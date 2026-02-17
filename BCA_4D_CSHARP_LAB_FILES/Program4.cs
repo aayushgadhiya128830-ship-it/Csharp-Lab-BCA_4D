a22bfa46f1a147ff33043d53ac7241e377f5c242
Aayush Gadhiya <aayush.gadhiya128830@marwadiuniversity.ac.in>
2026-02-17 18:15:47 +0530

    Added Csharp programs in folder

---
+++ BCA_4D_CSHARP_LAB_FILES/Program4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Relational
+{
+    class Relational
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre The Number For A : ");
+            int A = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre The Number For B : ");
+            int B = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(A + "==" + B + "is" + (A == B));
+            Console.WriteLine(A + "!=" + B + "is" + (A != B));
+            Console.WriteLine(A + "<" + B + "is" + (A < B));
+            Console.WriteLine(A + ">" + B + "is" + (A > B));
+            Console.WriteLine(A + "<=" + B + "is" + (A <= B));
+            Console.WriteLine(A + ">=" + B + "is" + (A >= B));
+        }
+    }
+}
