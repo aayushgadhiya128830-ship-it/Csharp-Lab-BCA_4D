569f6c598f16e6c7f7359fafef855cb24fd7d71f
Aayush Gadhiya <aayush.gadhiya128830@marwadiuniversity.ac.in>
2026-03-16 09:29:09 +0530

    Add files via upload
    New Program

---
+++ BCA_4D_CSHARP_LAB_FILES/Program25.cs
@@ -0,0 +1,27 @@
+﻿// Write a program to display minimum number from inputted 5 numbers from user.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num, min;
+
+        Console.Write("Enter number 1: ");
+        min = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 2; i <= 5; i++)
+        {
+            Console.Write("Enter number " + i + ": ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num < min)
+            {
+                min = num;
+            }
+        }
+
+        Console.WriteLine("Minimum number is: " + min);
+    }
+}
