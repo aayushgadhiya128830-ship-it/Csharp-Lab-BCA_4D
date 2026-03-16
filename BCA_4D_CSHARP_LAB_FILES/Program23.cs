eb445c37473d7b4e9ca9956dbf5d0a51edc797c9
Aayush Gadhiya <aayush.gadhiya128830@marwadiuniversity.ac.in>
2026-03-16 09:27:46 +0530

    Add files via upload
    New Program

---
+++ BCA_4D_CSHARP_LAB_FILES/Program23.cs
@@ -0,0 +1,38 @@
+﻿// Write a program to verify PIN first, then allow withdrawal only if balance is sufficient.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int pin, enteredPin;
+        double balance = 5000, withdrawAmount;
+
+        pin = 1234;   // Correct PIN
+
+        Console.Write("Enter your PIN: ");
+        enteredPin = Convert.ToInt32(Console.ReadLine());
+
+        if (enteredPin == pin)
+        {
+            Console.Write("Enter withdrawal amount: ");
+            withdrawAmount = Convert.ToDouble(Console.ReadLine());
+
+            if (withdrawAmount <= balance)
+            {
+                balance = balance - withdrawAmount;
+                Console.WriteLine("Withdrawal Successful");
+                Console.WriteLine("Remaining Balance: " + balance);
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid PIN");
+        }
+    }
+}
