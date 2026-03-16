ff98c3c5a3ba66fc653e5a89dccdb00a629f224f
Aayush Gadhiya <aayush.gadhiya128830@marwadiuniversity.ac.in>
2026-03-16 09:28:36 +0530

    Add files via upload
    New Program

---
+++ BCA_4D_CSHARP_LAB_FILES/Program24.cs
@@ -0,0 +1,27 @@
+﻿// Write a program to display maximum number from inputted 5 numbers from user.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num, max;
+
+        Console.Write("Enter number 1: ");
+        max = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 2; i <= 5; i++)
+        {
+            Console.Write("Enter number " + i + ": ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+
+        Console.WriteLine("Maximum number is: " + max);
+    }
+}
