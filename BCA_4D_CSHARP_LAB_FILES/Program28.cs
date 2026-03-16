c04513ea82ddfa1f40831270cd333eb4fc21c629
Aayush Gadhiya <aayush.gadhiya128830@marwadiuniversity.ac.in>
2026-03-16 09:56:16 +0530

    Add files via upload
    New Program

---
+++ BCA_4D_CSHARP_LAB_FILES/Program28.cs
@@ -0,0 +1,57 @@
+﻿/* Create a class “Rectangle” that would contain length and
+width as an instance variable, define constructors
+[constructor overloading (default, parameterized)] to
+initialize variables of objects.Define methods to find area
+and to display variables’
+value of objects which are created. */
+
+using System;
+
+namespace Rectangle
+{
+    class Rectangle
+    {
+        double length;
+        double width;
+
+        public Rectangle()
+        {
+            length = 0;
+            width = 0;
+        }
+
+        public Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+
+        public double FindArea()
+        {
+            return length * width;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Length: " + length);
+            Console.WriteLine("Width: " + width);
+            Console.WriteLine("Area: " + FindArea());
+        }
+    }
+
+    class Program28
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle();
+            Console.WriteLine("REC 1 (Defult Con.) : ");
+            r1.Display();
+
+            Console.WriteLine();
+
+            Rectangle r2 = new Rectangle(10, 5);
+            Console.WriteLine("REC 2 (Perameterized Con.) : ");
+            r2.Display();
+        }
+    }
+}
