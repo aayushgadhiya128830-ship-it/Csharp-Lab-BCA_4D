b414b63bd4a86fd07403f946d7c60ef182ccd194
Aayush Gadhiya <aayush.gadhiya128830@marwadiuniversity.ac.in>
2026-04-07 08:39:36 +0530

    Add files via upload
    New Program....

---
+++ BCA_4D_CSHARP_LAB_FILES/Program34.cs
@@ -0,0 +1,41 @@
+﻿// write a program demonstrate the use of try , catch and finaly in c#.
+
+using System;
+
+namespace TryCatchFinally
+{
+    class TryCatchFinally
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Enter The 1st Number : ");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter The 2nd Number : ");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                int c = a / b;
+
+                Console.WriteLine("Divition Is : " + c);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Error : Cannot Be Zero Divied...");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error : Invalid Format, Please Enter Numeric Values...");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unexpected Error : " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Program Execution Complete...");
+            }
+        }
+    }
+}
