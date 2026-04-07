c5d49500a652a1bef95b3802ed0e343e6e160cb5
Aayush Gadhiya <aayush.gadhiya128830@marwadiuniversity.ac.in>
2026-04-07 08:25:10 +0530

    Add files via upload
    New Program.

---
+++ BCA_4D_CSHARP_LAB_FILES/Program33.cs
@@ -0,0 +1,43 @@
+﻿// Write a program demonstrate the use of try and catch in c#.
+
+using System;
+
+namespace TryCatch
+{
+    class TryCatch
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Enter The 1st Number : ");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter The 2nd Number : ");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                int c = a + b;
+
+                Console.WriteLine("Sum Is : " + c);
+
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Error : Cannot Divide By Zero...");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error : Invalid Input, Please Numeric Values...");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unexpected Error : " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Program Execute Complete...");
+            }
+
+        }
+    }
+}
